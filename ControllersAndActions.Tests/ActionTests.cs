0fe956691d286a1f82641a19cc4ab8ca2ae21aab
Вадим Зверев <WadimZverev@gmail.com>
2018-08-24 02:07:35 +0400

    Юнит тест: отображение представления

---
+++ ControllersAndActions.Tests/ActionTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ControllersAndActions.Controllers;
+using System.Web.Mvc;
+namespace ControllersAndActions.Tests
+{
+    [TestClass]
+    public class ActionTests
+    {
+        [TestMethod]
+        public void ViewSelectionTest()
+        {
+            // Arrange - создание контроллера
+            ExampleController target = new ExampleController();
+            // Act - вызов метода действия
+            ViewResult result = target.Index();
+            // Assert - проверка результата
+            Assert.AreEqual("Homepage", result.ViewName);
+        }
+    }
+}
