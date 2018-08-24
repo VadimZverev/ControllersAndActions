5ffe09ee9303ac215d27c76a08eab1cbd58a6416
VadimZverev <39337335+VadimZverev@users.noreply.github.com>
2018-08-24 11:01:00 +0400

    Предоставление объекта модели представления

--- ControllersAndActions/Controllers/ExampleController.cs
+++ ControllersAndActions/Controllers/ExampleController.cs
@@ -10,7 +10,8 @@ namespace ControllersAndActions.Controllers
     {
         public ViewResult Index()
         {
-            return View("Homepage");
+            DateTime date = DateTime.Now;
+            return View(date);
         }
     }
 }
