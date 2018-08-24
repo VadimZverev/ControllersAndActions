33c34f1e7ac44f18c3dced668b140c4c77ca54b9
VadimZverev <39337335+VadimZverev@users.noreply.github.com>
2018-08-24 11:22:09 +0400

    Юнит тест: ViewBag

--- ControllersAndActions.Tests/ActionTests.cs
+++ ControllersAndActions.Tests/ActionTests.cs
@@ -12,10 +12,25 @@ namespace ControllersAndActions.Tests
         {
             // Arrange - создание контроллера
             ExampleController target = new ExampleController();
+
             // Act - вызов метода действия
             ViewResult result = target.Index();
+
             // Assert - проверка результата
             Assert.AreEqual("Hello, World", result.ViewData.Model);
+        }
+
+        [TestMethod]
+        public void ControllerTest()
+        {
+            // Arrange - создание контроллера
+            ExampleController target = new ExampleController();
+
+            // Act - вызов метода действия
+            ViewResult result = target.Index();
+
+            // Assert - проверка результата
+            Assert.AreEqual("Hello", result.ViewBag.Message);
         }
     }
 }
